f38d5e34e3bd80b1559be817ab7d271fdd942b13
DanishQ1011 <officiallyshah@outlook.com>
2024-08-28 11:40:04 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const ibmPlexSerif = IBM_Plex_Serif({ subsets: ["latin"], weight: ['400', '700'], variable: "--font-ibm-plex-serif" });
+
+export const metadata: Metadata = {
+  title: "Leonidas",
+  description: "A Fintech banking transaction processing web application project",
+  icons: {
+    icon: "/icons/logo.svg"
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
