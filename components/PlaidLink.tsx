9bdcc3ca0a62a1f77b2b1b4b9c88649c14eaa072
DanishQ1011 <officiallyshah@outlook.com>
2024-09-06 19:07:51 +0530

    Implement Connect bank feature within the sidebar of dashboard

--- components/PlaidLink.tsx
+++ components/PlaidLink.tsx
@@ -3,6 +3,7 @@ import { Button } from './ui/button'
 import { PlaidLinkOnSuccess, PlaidLinkOptions, usePlaidLink } from 'react-plaid-link'
 import { useRouter } from 'next/navigation';
 import { createLinkToken, exchangePublicToken } from '@/lib/actions/user.actions';
+import Image from 'next/image';
 
 const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
     const router = useRouter();
@@ -43,12 +44,24 @@ const PlaidLink = ({ user, variant }: PlaidLinkProps) => {
                     Connect Bank
             </Button>
         ) : variant === 'ghost' ? (
-            <Button variant='ghost'>
-                Connect Bank
+            <Button variant='ghost' onClick={() => open()} className='plaidlink-ghost'>
+                <Image
+                    src='/icons/connect-bank.svg'
+                    width={24}
+                    height={24}
+                    alt='connect bank'
+                />
+                <p className='hidden text-[16px] font-semibold text-black-2 xl:block'>Connect Bank</p>
             </Button>
         ) : (
-            <Button>
-                Connect Bank
+            <Button onClick={() => open()} className='plaidlink-default'>
+                <Image
+                    src='/icons/connect-bank.svg'
+                    width={24}
+                    height={24}
+                    alt='connect bank'
+                />
+                <p className='text-[16px] font-semibold text-black-2'>Connect Bank</p>
             </Button>
         )}
     </>
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer';
+import PlaidLink from './PlaidLink';
 
 const Sidebar = ({ user }: SiderbarProps) => {
     const pathname = usePathname();
@@ -46,7 +47,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
                     </Link>
                 )
             })}
-            USER
+            <PlaidLink user={user} />
         </nav>
            <Footer user={user} type='mobile' /> 
     </section>
